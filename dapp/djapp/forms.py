2337e63f32425d3c9de555807f4ba830c8f62d38
kuroneko <kskkm0228@gmail.com>
2019-10-18 17:55:38 +0900

    bodyを大きくしておきました

--- dapp/djapp/forms.py
+++ dapp/djapp/forms.py
@@ -3,5 +3,4 @@ from django import forms
 class kakikomiForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
-    body = forms.CharField()
-    
+    body = forms.CharField(widget=forms.Textarea)
