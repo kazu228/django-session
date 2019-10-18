3e1237f89cd628edfb4717027d6fdafecca102ee
kuroneko <kskkm0228@gmail.com>
2019-10-18 18:01:05 +0900

    urls.pyにアプリ名を追記

--- dapp/djapp/urls.py
+++ dapp/djapp/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 app_name = 'djapp'
 
 urlpatterns = [
-    url('', views.kakikomi, name='kakikomi'),
+    path('', views.kakikomi, name='kakikomi'),
 ]
