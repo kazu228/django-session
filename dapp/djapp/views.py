2848b9a68d2edf55b27448d81a918dc1fefb79ac
kuroneko <kskkm0228@gmail.com>
2019-10-18 18:16:23 +0900

    add user_data_confirm関数

--- dapp/djapp/views.py
+++ dapp/djapp/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+#from django.http import HttpResponse
 from .forms import kakikomiForm
 # Create your views here.
 
 def kakikomi(request):
-    f = kakikomiForm()
+    if request.method == 'GET':
+        f = kakikomiForm(request.session.get('form_data'))
+    else:
+        f = kakikomiForm(request.POST)
+        if f.is_valid:
+            request.session['form_data'] = request.POST
+            return redirect('djapp:user_data_confirm')
     return render(request, 'djapp/index.html', {'form1': f})
 
+
+def user_data_confirm(request):
+    session_form_data = request.session.get('form_data')
+    if session_form_data is None:
+        return redirect('djapp:kakikomi')
+    
+    context = {
+        'form': kakikomiForm(session_form_data)
+    }
+    return render(request, 'djapp/user_data_confirm', context)
