8f13543dba1274ac58046405fa5db8e7b5930831
kuroneko <kskkm0228@gmail.com>
2019-10-18 17:20:48 +0900

    kakikomi関数を定義

--- dapp/djapp/views.py
+++ dapp/djapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .forms import kakikomiForm
 # Create your views here.
+
+def kakikomi(request):
+    f = kakikomiForm()
+    return HttpResponse(f)
+
