f075b4ad45395203af1ed91250ab4f0714526bc0
kuroneko <kskkm0228@gmail.com>
2019-10-20 18:43:28 +0900

    add user_data_create関数

--- dapp/djapp/views.py
+++ dapp/djapp/views.py
@@ -26,3 +26,20 @@ def user_data_confirm(request):
         # フォーム形式で出力するため
     }
     return render(request, 'djapp/user_data_confirm.html', context)
+
+def user_data_create(request):
+    session_form_data = request.session.pop('form_data', None)
+    # セッションを空にして取り出す
+
+    if session_form_data is None:
+        return redirect('djapp:user_data_input')
+
+    form = kakikomiForm(session_form_data)
+    if form.is_valid():
+        form.save()
+        return redirect('djapp:user_list')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'djapp/user_data_input.html', context)
